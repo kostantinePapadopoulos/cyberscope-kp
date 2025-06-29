9a592543a8c22e25194204a03488303d832507db
konstantinePapadopoulos <kpap@qnr.com.gr>
2025-06-29 20:43:41 +0300

    centered tablelist

--- components/pageComponents/MainPage/MainPageContainer.jsx
+++ components/pageComponents/MainPage/MainPageContainer.jsx
@@ -6,7 +6,9 @@ import { useRouter } from "next/navigation";
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+
   gap: 1rem;
+  max-width: 1400px;
 `;
 
 const MainPageContainer = () => {
@@ -14,21 +16,25 @@ const MainPageContainer = () => {
 
   return (
     <Container>
-      <DataTable
-        url="/api/coins/markets"
-        columns={[
-          { key: "name", label: "Name" },
-          { key: "symbol", label: "Symbol" },
-          { key: "current_price", label: "Current price" },
-          { key: "high_24h", label: "Highest price last 24h" },
-          { key: "low_24h", label: "Lowest price last 24h" },
-          {
-            key: "price_change_percentage_24h",
-            label: "Price change last 24h",
-          },
-        ]}
-        onRowClick={(row) => router.push(`/coins/${row.id}`)}
-      />
+      <div
+        style={{ width: "100vw", display: "flex", justifyContent: "center" }}
+      >
+        <DataTable
+          url="/api/coins/markets"
+          columns={[
+            { key: "name", label: "Name" },
+            { key: "symbol", label: "Symbol" },
+            { key: "current_price", label: "Current price" },
+            { key: "high_24h", label: "Highest price last 24h" },
+            { key: "low_24h", label: "Lowest price last 24h" },
+            {
+              key: "price_change_percentage_24h",
+              label: "Price change last 24h",
+            },
+          ]}
+          onRowClick={(row) => router.push(`/coins/${row.id}`)}
+        />
+      </div>
     </Container>
   );
 };
